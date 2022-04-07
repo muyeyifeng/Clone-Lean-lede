9dc0b371195d4a223811ba8d0010502d404dd2ab
muyeyifeng <muyeyifeng@gmail.com>
2022-04-07 21:31:44 +0000

    Clone lean github source

--- feeds/freifunk.tmp/info/.files-packageinfo.mk
+++ feeds/freifunk.tmp/info/.files-packageinfo.mk
@@ -1,6 +1,5 @@
 $(eval $(call PackageDir,applications_luci-app-freifunk-diagnostics,applications/luci-app-freifunk-diagnostics,))
 $(eval $(call PackageDir,applications_luci-app-freifunk-policyrouting,applications/luci-app-freifunk-policyrouting,))
-$(eval $(call PackageDir,applications_luci-app-freifunk-widgets,applications/luci-app-freifunk-widgets,))
 $(eval $(call PackageDir,applications_luci-app-meshwizard,applications/luci-app-meshwizard,))
 $(eval $(call PackageDir,applications_luci-app-owm,applications/luci-app-owm,))
 $(eval $(call PackageDir,contrib_package_community-profiles,contrib/package/community-profiles,))
--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,11 +6,12 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .162
-LINUX_VERSION-5.10 = .84
+KERNEL_DETAILS_FILE=$(INCLUDE_DIR)/kernel-$(KERNEL_PATCHVER)
+ifeq ($(wildcard $(KERNEL_DETAILS_FILE)),)
+  $(error Missing kernel version/hash file for $(KERNEL_PATCHVER). Please create $(KERNEL_DETAILS_FILE))
+endif
 
-LINUX_KERNEL_HASH-5.4.162 = c12d72ddaac78189305a5e98825295ecb02282970033b052276035e83189e25b
-LINUX_KERNEL_HASH-5.10.84 = 6e96271d72dd106c4bd83931ca16fda622413b34c5649e2916116c38c12e233d
+include $(KERNEL_DETAILS_FILE)
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
